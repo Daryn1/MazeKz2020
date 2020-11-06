2f6b2a49844527aea176edf10f2a60c59c4f8964
Sultan <hasenovsultanbek@gmail.com>
2020-11-06 11:57:51 +0600

    added Khassenov page

---
+++ WebMaze/Controllers/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMaze.Models;
+
+namespace WebMaze.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public IActionResult Khassenov()
+        {
+            var heroes = new List<HeroViewModel>()
+            {
+                new HeroViewModel(){Name = "Knight", HP = 625, Mana = 150.0, Properties = new List<string>(){"Solid steel", "Sharp sword"}},
+                new HeroViewModel(){Name = "Mage", HP = 587, Mana = 175.5, Properties = new List<string>(){"Metal fists"}},
+                new HeroViewModel(){Name = "Alchemist",HP = 804, Mana = 100, Properties = new List<string>(){"Brutal view", "Axe attacks"}},
+            };
+            return View(heroes);
+        }
+    }
+}
---
+++ WebMaze/Models/HeroViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMaze.Models
+{
+    public class HeroViewModel
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public double Mana { get; set; }
+        public List<string> Properties { get; set; }
+    }
+}
