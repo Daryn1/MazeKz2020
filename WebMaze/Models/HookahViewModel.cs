4a5d78930abbd897e4b1e5445eb35a7975fe145d
MaksimMochalkin <Maksimmochalkin@gmail.com>
2020-11-20 15:46:55 +0600

    Changes

--- WebMaze/Models/HookahViewModel.cs
+++ WebMaze/Models/HookahViewModel.cs
@@ -8,7 +8,7 @@ namespace WebMaze.Models
     public class HookahViewModel
     {
         public string Name { get; set; }
-        public decimal? Price { get; set; }
+        public decimal Price { get; set; }
         public string Material { get; set; }
         public string ManufacturerCountry { get; set; }
         public decimal Height { get; set; }
