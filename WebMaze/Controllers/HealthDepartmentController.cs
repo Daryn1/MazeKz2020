6e57c7e50080f3c0cbd6cbf0c2606aca9c209b83
Максим Мочалкин <maksimmochalkin@gmail.com>
2020-12-09 17:01:57 +0600

    Add Page Medicine
    Добавлена страница медицины, в Home/Index.cshtml добавлена ссылка в виде картинки

---
+++ WebMaze/Controllers/HealthDepartmentController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebMaze.DbStuff.Repository;
+
+namespace WebMaze.Controllers
+{
+    public class HealthDepartmentController : Controller
+    {
+        private HealthDepartmentRepository departmentRepository;
+        private IMapper mapper;
+
+
+        public HealthDepartmentController(HealthDepartmentRepository departmentRepository, IMapper mapper)
+        {
+            this.departmentRepository = departmentRepository;
+            this.mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public IActionResult HealthDepartment()
+        {
+            return View();
+        }
+    }
+}
