963b4cf4e22a1cba4ad84975098a32191747eb86
Sultan <hasenovsultanbek@gmail.com>
2020-12-09 16:08:40 +0600

    Housing core layer

---
+++ Housing.Core/Models/House.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Housing.Core.Models
+{
+    public class House
+    {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+}
